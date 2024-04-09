910be4d9f84191464f8ee84097e9777b535afc4f
Pedro Manuel Montes Valle <141801243+PedroMMV@users.noreply.github.com>
2024-04-09 00:30:47 +0000

    Add Polygon class and update main function in exercise.cpp

---
+++ assignments/00-ShapeCircleRectanglePolygon/Polygon.cpp
@@ -0,0 +1,17 @@
+#include "Polygon.hpp"
+
+Polygon::Polygon()
+{
+  sides = 0;
+}
+
+Polygon::Polygon(int valX, int valY, int valSides) : Shape{valX, valY}
+{
+  sides = valSides;
+}
+
+int Polygon::getSides()
+{
+  return sides;
+}
+
